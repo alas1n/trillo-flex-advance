0ba194a5676d65de4a140fc865ec274cfbad03e7
ali <amirzaei26@gmail.com>
2020-11-07 15:57:37 +0330

    added new page

---
+++ src/pages/mamandsPages.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const MamadsPage = () => {
+    return (
+        <div>
+            {/* <p>{hotlenmae}</p>
+            <p>{hotlenmae}</p> */}
+            mamads page
+        </div>
+    )
+}
+
+
+export default MamadsPage
