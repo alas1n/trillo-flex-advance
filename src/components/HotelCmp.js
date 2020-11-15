8d0814d128e3432a73a25986d36ea1d71efe242d
ali <amirzaei26@gmail.com>
2020-11-15 16:59:09 +0330

    fetch images form server in hotel list cmp

--- src/components/HotelCmp.js
+++ src/components/HotelCmp.js
@@ -1,21 +1,59 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 // Assets:
-import hotelImage from "./../assets/SampleHotel.jpg";
+import hotelImage from "./../assets/hotel-placeholder.png";
 
 const HotelCmp = (props) => {
-  const image = props.img;
-  console.log(typeof image);
-  const hotelImp = props.img === "" ? hotelImage : props.img;
+  const [images, setImages] = useState([]);
+  const [urls, setUrls] = useState(props.img);
+
+  const fetchData = (urls) => {
+    urls.map(async (url) => {
+      try {
+        const res = await fetch(`http://${url}`);
+        if (res.status === 200) {
+          // console.log(res.status);
+          console.log(url);
+          setImages(`http://${url}`);
+        } else {
+          setImages(hotelImage);
+          console.log(res.status);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    });
+  };
+
+  useEffect(() => {
+    if (urls) {
+      fetchData(urls);
+    }
+    return () => {
+      // cleanup;
+    };
+  }, []);
+
   return (
     <div className="HotelCmp">
-      <img src={hotelImp} alt="" className="HotelCmp_img" />
+      <div className="HotelCmp_img_wrapper">
+        <img
+          src={images}
+          alt=""
+          className="HotelCmp_img_wrapper_img"
+          // style={{ backgroundImage: urls(images) }}
+        />
+      </div>
       <div className="HotelCmp-detail">
         <div className="HotelCmp-detail_name-rate">
           <p className="HotelCmp-detail_name-rate--name">{props.hotelName}</p>
           <p className="HotelCmp-detail_name-rate--rate">{props.hotelRating}</p>
         </div>
-        <p className="HotelCmp-detail-rating_rating">{props.hotelAdress}</p>
+        <address className="HotelCmp-detail-rating_rating">
+          {props.hotelAdress}
+        </address>
+        {/* <p className="HotelCmp-detail-rating_rating">{props.hotelAdress}</p> */}
       </div>
     </div>
   );
