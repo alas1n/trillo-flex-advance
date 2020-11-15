08965f76c44ced59725480756bbf98c92c6a9673
ali <amirzaei26@gmail.com>
2020-11-15 17:45:46 +0330

    added stars for hotel

--- src/components/HotelCmp.js
+++ src/components/HotelCmp.js
@@ -1,8 +1,13 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import Repeat from "react-repeat-component";
+
+// import axios from "axios";
 
 // Assets:
 import hotelImage from "./../assets/hotel-placeholder.png";
+
+// ICONS
+import { FaRegStar } from "react-icons/fa";
 
 const HotelCmp = (props) => {
   const [images, setImages] = useState([]);
@@ -48,12 +53,20 @@ const HotelCmp = (props) => {
       <div className="HotelCmp-detail">
         <div className="HotelCmp-detail_name-rate">
           <p className="HotelCmp-detail_name-rate--name">{props.hotelName}</p>
-          <p className="HotelCmp-detail_name-rate--rate">{props.hotelRating}</p>
+          {/* <p className="HotelCmp-detail_name-rate--rate">{props.hotelRating}</p> */}
+          <Repeat times={props.hotelRating}>
+            {(i) => (
+              <FaRegStar
+                key={i}
+                id={`step-${i}`}
+                className="HotelCmp-detail_name-rate--icon"
+              ></FaRegStar>
+            )}
+          </Repeat>
         </div>
         <address className="HotelCmp-detail-rating_rating">
           {props.hotelAdress}
         </address>
-        {/* <p className="HotelCmp-detail-rating_rating">{props.hotelAdress}</p> */}
       </div>
     </div>
   );
