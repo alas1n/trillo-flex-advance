9970cce1ddb49465b1324b236f46c33ef8a04a83
ali <amirzaei26@gmail.com>
2020-11-16 15:16:49 +0330

    lazy scroll oke

--- src/components/HotelCmp.js
+++ src/components/HotelCmp.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Repeat from "react-repeat-component";
 
-// import axios from "axios";
-
 // Assets:
 import hotelImage from "./../assets/hotel-placeholder.png";
 
@@ -15,19 +13,21 @@ const HotelCmp = (props) => {
 
   const fetchData = (urls) => {
     urls.map(async (url) => {
-      try {
-        const res = await fetch(`http://${url}`);
-        if (res.status === 200) {
-          // console.log(res.status);
-          console.log(url);
-          setImages(`http://${url}`);
-        } else {
-          setImages(hotelImage);
-          console.log(res.status);
-        }
-      } catch (error) {
-        console.log(error);
-      }
+      await fetch(`http://${url}`)
+        .then((response) => {
+          if (response.ok) {
+            setImages(`http://${url}`);
+          } else {
+            setImages(hotelImage);
+          }
+          throw new Error("Network response was not ok.");
+        })
+        .catch(function (error) {
+          console.log(
+            "There has been a problem with your fetch operation: ",
+            error.message
+          );
+        });
     });
   };
 
@@ -42,27 +42,24 @@ const HotelCmp = (props) => {
 
   return (
     <div className="HotelCmp">
+      <p className="HotelCmp-detail_name-rate--name">{props.num}</p>
       <div className="HotelCmp_img_wrapper">
-        <img
-          src={images}
-          alt=""
-          className="HotelCmp_img_wrapper_img"
-          // style={{ backgroundImage: urls(images) }}
-        />
+        <img src={images} alt="" className="HotelCmp_img_wrapper_img" />
       </div>
       <div className="HotelCmp-detail">
         <div className="HotelCmp-detail_name-rate">
           <p className="HotelCmp-detail_name-rate--name">{props.hotelName}</p>
-          {/* <p className="HotelCmp-detail_name-rate--rate">{props.hotelRating}</p> */}
-          <Repeat times={props.hotelRating}>
-            {(i) => (
-              <FaRegStar
-                key={i}
-                id={`step-${i}`}
-                className="HotelCmp-detail_name-rate--icon"
-              ></FaRegStar>
-            )}
-          </Repeat>
+          {props.hotelRating ? (
+            <Repeat times={props.hotelRating}>
+              {(i) => (
+                <FaRegStar
+                  key={i}
+                  id={`step-${i}`}
+                  className="HotelCmp-detail_name-rate--icon"
+                ></FaRegStar>
+              )}
+            </Repeat>
+          ) : undefined}
         </div>
         <address className="HotelCmp-detail-rating_rating">
           {props.hotelAdress}
