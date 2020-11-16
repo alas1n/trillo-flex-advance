86897d7c28443d087452223f8a61c37dd5f0c6d0
ali <amirzaei26@gmail.com>
2020-11-16 15:17:52 +0330

    noting

--- src/Api/config.js
+++ src/Api/config.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: `http://d93152030961.ngrok.io/api/v1/hotel/hotels`,
+  baseURL: `http://3a564ad9296c.ngrok.io/api/v1/hotel/hotels`,
 });
